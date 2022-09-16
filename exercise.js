5adba1d10a57f4d94359e29572d27247d0a75c0e
aiman99aleryany <102668388+aiman99aleryany@users.noreply.github.com>
2022-09-17 01:38:13 +0300

    init commit

---
+++ exercise.js
@@ -0,0 +1,21 @@
+// # Exercise 17
+
+// Rewrite the exercise number 15, by using the `if else` statement.
+
+// let isSunnyDay = true;
+// let isHomeworkCompleted = false;
+
+// console.log(
+//   isSunnyDay && isHomeworkCompleted
+//     ? "Jesse can go out to play"
+//     : "Jesse stays home"
+// );
+
+let isSunnyDay = true;
+let isHomeworkCompleted = true;
+
+if (isSunnyDay && isHomeworkCompleted) {
+  console.log("Jesse can go out to play");
+} else {
+  console.log("Jesse stays home");
+}
